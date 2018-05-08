2d36c695275269d74d7b20ba72b2e6bb20ed16d0
Ricardo Villeda <Ricardo Villeda@192.168.1.8>
2018-05-07 20:46:08 -0600

    Metodo nueva reservacion, con sus validaciones, costo por noche y disponibilidad

--- src/parcialpoonarnia/Cliente.java
+++ src/parcialpoonarnia/Cliente.java
@@ -108,4 +108,103 @@ public class Cliente {
         this.Dui = leer.nextLine();
         this.infoCliente = "Nombre: " + this.nombre + " Apellido: " + this.apellido + " DUI: " + this.Dui + " Telefono: " + this.telefono + " Tarjeta de credito: " + this.tarjetaCredito;
     }
+    
+    /////////////////////////////Nueva reservacion///////////////////////////////////////////////////
+    public void NuevaReservacion(String nombrePaquete, String pisoHabitacion, int numeroHabitacion){
+        Reservacion nuevaReservacion = new Reservacion();      
+        double costo, costoT;
+        int fecha = 0, diaReservacion = 0;
+        Scanner n = new Scanner(System.in);
+        boolean bandera = true;
+        while(bandera){
+            System.out.println("Dia en que desea hacer la reserva(Tomar en cuenta que ahora es 1 de Enero): ");
+            fecha = n.nextInt();
+            if(fecha>0 && fecha<31){
+                if(VerificarDisponibilidad(pisoHabitacion,numeroHabitacion,fecha)){
+                   bandera = false;
+                }
+                else{
+                    System.out.println("La habitacion no esta disponible ese dia. ");
+                }
+            }
+            else{
+                System.out.println("Por el momento solo se puede reservar hasta el 30 de Enero");
+            }
+        }
+        while(!bandera){
+            System.out.println("Cuantos dias piensa quedarse (El maximo son 7 días): ");
+            diaReservacion = n.nextInt();
+            int dias = 0;
+            if(diaReservacion>0 && diaReservacion<8){
+                for(int x=0;x<diaReservacion;x++){
+                    if(VerificarDisponibilidad(pisoHabitacion,numeroHabitacion,fecha+x)){   
+                        dias++;
+                    }
+                    else{
+                        System.out.println("La habitacion que desea no esta disponible el dia "+(fecha+x));
+                    }
+                }
+                if(dias == diaReservacion){
+                    for(int x=0;x<diaReservacion;x++){
+                        CambiarDisponibilidad(pisoHabitacion,numeroHabitacion,fecha+x);
+                    }
+                    bandera = true;
+                }
+            }
+        }
+        costo = CostoPorNoche(pisoHabitacion,numeroHabitacion,nombrePaquete);
+        costoT = diaReservacion*costo;
+        nuevaReservacion.setDiasReservacion(diaReservacion);
+        nuevaReservacion.setFecha(fecha);
+        nuevaReservacion.setInfoHuesped(infoCliente);
+        nuevaReservacion.setNombrePaquete(nombrePaquete);
+        nuevaReservacion.setNumeroHabitacion(numeroHabitacion);
+        nuevaReservacion.setPisoHabitacion(pisoHabitacion);
+        nuevaReservacion.setCostoNoche(costo);
+        nuevaReservacion.setCostoTotal(costoT);
+        Reservaciones.add(nuevaReservacion);
+    }
+    
+    public double CostoPorNoche(String pisoHabitacion,int numeroHabitacion,String NombrePaquete){
+        double costoPaquete = 0,costoHabitacion = 0;
+        for(Paquete paquetes: Paquetes){
+            if(paquetes.getNombrePaquete().equals(NombrePaquete)){
+                costoPaquete = paquetes.getPrecioPaquete();
+            }
+        }
+        for(Habitacion habitaciones:Habitaciones){
+            if(habitaciones.getNumero() == numeroHabitacion && habitaciones.getPiso().equals(pisoHabitacion)){
+                costoHabitacion = habitaciones.getPrecioHabitacion();
+            }
+        }
+        return (costoPaquete + costoHabitacion);
+    }
+    
+    public boolean VerificarReservacion(){
+        int cont=0;
+        for(Reservacion reservar: Reservaciones){
+            cont++;
+        }
+        return (cont == 0 || cont == 1);
+    }
+    
+    public boolean VerificarDisponibilidad(String pisoHabitacion,int numeroHabitacion,int dia){
+        for(Habitacion h: Habitaciones){
+            if(h.getNumero() == numeroHabitacion && h.getPiso().equals(pisoHabitacion)){
+                return ("Disponible".equals(h.DisponibilidadMes.get(dia)));
+            }
+        }
+        return false;
+    }
+    
+    public void CambiarDisponibilidad(String pisoHabitacion,int numeroHabitacion,int dia){
+        for(Habitacion h: Habitaciones){
+            if(h.getNumero() == numeroHabitacion && h.getPiso().equals(pisoHabitacion)){
+                h.DisponibilidadMes.set(dia - 1,"No disponible");
+            }
+        }
+    }
+    
+    
 }
+
